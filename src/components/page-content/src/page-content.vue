55fd292b9c7a8a490277ba74994bf27c36784c23
liux <liux1118@qq.com>
2021-09-24 08:53:34 +0800

    feat(user): pageContent的抽取

---
+++ src/components/page-content/src/page-content.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="page-content">
+    <lx-table :listData="userList" v-bind="contentTableConfig">
+      <!-- 1.hander中的插槽 -->
+      <template #handerHandler>
+        <el-button size="medium" icon="el-icon-refresh">刷新</el-button>
+        <el-button size="medium" type="primary" @click="handleNewUser"
+          >新建用户</el-button
+        >
+      </template>
+
+      <!-- 2.列中的插槽 -->
+      <template #status="scope">
+        <el-button
+          size="mini"
+          :type="scope.row.enable ? 'success' : 'danger'"
+          plain
+          >{{ scope.row.enable ? '启用' : '禁用' }}</el-button
+        >
+      </template>
+      <template #createAt="scope">
+        <!-- 时间格式化 -->
+        <span>{{ $filters.formatTime(scope.row.createAt) }}</span>
+      </template>
+      <template #updateAt="scope">
+        <span>{{ $filters.formatTime(scope.row.updateAt) }}</span>
+      </template>
+      <template #handler>
+        <div class="hander-btns">
+          <el-button size="mini" type="text" icon="el-icon-edit"
+            >编辑</el-button
+          >
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            style="color: red"
+            >删除</el-button
+          >
+        </div>
+      </template>
+    </lx-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+// 自己的useStore
+import { useStore } from '@/store'
+
+import LxTable from '@/base-ui/table'
+
+export default defineComponent({
+  props: {
+    contentTableConfig: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    LxTable
+  },
+  setup() {
+    // 网络请求
+    const store = useStore()
+    store.dispatch('systemModule/getPageListAction', {
+      pageUrl: '/users/list',
+      // 查询条件
+      queryInfo: {
+        offset: 0, //偏移量
+        size: 10 // 一个页面展示10条数据
+      }
+    })
+
+    const userList = computed(() => store.state.systemModule.userList)
+    const userCount = computed(() => store.state.systemModule.userCount)
+    return {
+      userList,
+      userCount
+    }
+  }
+})
+</script>
+
+<style scoped>
+.page-content {
+  padding: 20px;
+  border-top: 20px solid #f5f5f5;
+}
+</style>
--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,130 +1,32 @@
 <template>
   <div class="user">
     <page-search :searchFormConfig="searchFormConfig" />
-    <div class="content">
-      <lx-table
-        :listData="userList"
-        :title="title"
-        :propList="propList"
-        :showIndexColumn="showIndexColumn"
-        :showSelectColum="showSelectColum"
-      >
-        <!-- 1.hander中的插槽 -->
-        <template #handerHandler>
-          <el-button size="medium" icon="el-icon-refresh">刷新</el-button>
-          <el-button size="medium" type="primary" @click="handleNewUser"
-            >新建用户</el-button
-          >
-        </template>
-
-        <!-- 2.列中的插槽 -->
-        <template #status="scope">
-          <el-button
-            size="mini"
-            :type="scope.row.enable ? 'success' : 'danger'"
-            plain
-            >{{ scope.row.enable ? '启用' : '禁用' }}</el-button
-          >
-        </template>
-        <template #createAt="scope">
-          <!-- 时间格式化 -->
-          <span>{{ $filters.formatTime(scope.row.createAt) }}</span>
-        </template>
-        <template #updateAt="scope">
-          <span>{{ $filters.formatTime(scope.row.updateAt) }}</span>
-        </template>
-        <template #handler>
-          <div class="hander-btns">
-            <el-button size="mini" type="text" icon="el-icon-edit"
-              >编辑</el-button
-            >
-            <el-button
-              size="mini"
-              type="text"
-              icon="el-icon-delete"
-              style="color: red"
-              >删除</el-button
-            >
-          </div>
-        </template>
-      </lx-table>
-    </div>
+    <page-content :contentTableConfig="contentTableConfig" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue'
-
-import { useStore } from '@/store'
+import { defineComponent } from 'vue'
 
 import PageSearch from '@/components/page-search'
-import LxTable from '@/base-ui/table'
+import PageContent from '@/components/page-content'
 
 import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
 
 export default defineComponent({
   name: 'user',
   components: {
     PageSearch,
-    LxTable
+    PageContent
   },
   setup() {
-    const store = useStore()
-    store.dispatch('systemModule/getPageListAction', {
-      pageUrl: '/users/list',
-      // 查询条件
-      queryInfo: {
-        offset: 0, //偏移量
-        size: 10 // 一个页面展示10条数据
-      }
-    })
-
-    const userList = computed(() => store.state.systemModule.userList)
-    const userCount = computed(() => store.state.systemModule.userCount)
-
-    const title = '用户列表'
-
-    const propList = [
-      { prop: 'name', label: '用户名', minWidth: '100' },
-      { prop: 'realname', label: '真实姓名', minWidth: '100' },
-      { prop: 'cellphone', label: '手机号码', minWidth: '100' },
-      { prop: 'enable', label: '状态', minWidth: '100', slotName: 'status' },
-      {
-        prop: 'createAt',
-        label: '创建时间',
-        minWidth: '250',
-        slotName: 'createAt'
-      },
-      {
-        prop: 'updateAt',
-        label: '更新时间',
-        minWidth: '250',
-        slotName: 'updateAt'
-      },
-      { label: '操作', minWidth: '120', slotName: 'handler' }
-    ]
-
-    // 是否显示序号
-    const showIndexColumn = true
-    // 是否显示选中框
-    const showSelectColum = true
-
     return {
       searchFormConfig,
-      userList,
-      userCount,
-      title,
-      propList,
-      showIndexColumn,
-      showSelectColum
+      contentTableConfig
     }
   }
 })
 </script>
 
-<style scoped>
-.content {
-  padding: 20px;
-  border-top: 20px solid #f5f5f5;
-}
-</style>
+<style scoped></style>
