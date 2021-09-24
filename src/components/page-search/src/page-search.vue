02ed42dd3e226ae0b767abc80d642b408b8ce9b5
liux <liux1118@qq.com>
2021-09-24 12:28:45 +0800

    feat(pagesearch): 数据二次处理及重置按钮实现

--- src/components/page-search/src/page-search.vue
+++ src/components/page-search/src/page-search.vue
@@ -7,7 +7,9 @@
       </template>
       <template #footer>
         <div class="handle-btns">
-          <el-button icon="el-icon-refresh">重置</el-button>
+          <el-button icon="el-icon-refresh" @click="handleResetClick"
+            >重置</el-button
+          >
           <el-button type="primary" icon="el-icon-search">搜索</el-button>
         </div>
       </template>
@@ -29,16 +31,32 @@ export default defineComponent({
   components: {
     LxForm
   },
-  setup() {
-    const formData = ref({
-      id: '',
-      name: '',
-      status: '',
-      createTime: ''
-    })
+  setup(props) {
+    // 双向绑定的属性应该是由配置文件的field来决定的
+    // 1.优化一：formData中的属性应该动态决定
+    const formItems = props.searchFormConfig?.formItems ?? []
+    const formOriginData: any = {}
+    // item拿到的是一个个对象
+    for (const item of formItems) {
+      formOriginData[item.field] = ''
+    }
+    const formData = ref(formOriginData)
+    // 这是之前有所欠缺的写法
+    // const formData = ref({
+    //   id: '',
+    //   name: '',
+    //   status: '',
+    //   createTime: ''
+    // })
+
+    // 2.优化二：重置按钮
+    const handleResetClick = () => {
+      formData.value = formOriginData
+    }
 
     return {
-      formData
+      formData,
+      handleResetClick
     }
   }
 })
