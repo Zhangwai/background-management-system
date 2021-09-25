2a1e390b674b84a90e93f4d71bcaceb9cd241ef4
liux <liux1118@qq.com>
2021-09-25 21:40:11 +0800

    feat(category): 商品分类页面构建

--- src/views/main/product/category/category.vue
+++ src/views/main/product/category/category.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="category">
-    <h2>category</h2>
+    <page-search :searchFormConfig="searchFormConfig" />
+    <page-content
+      :contentTableConfig="contentTableConfig"
+      pageName="category"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import PageContent from '@/components/page-content'
+import pageSearch from '@/components/page-search'
+
+import { searchFormConfig } from './config/search.config'
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   name: 'category',
+  components: {
+    pageSearch,
+    PageContent
+  },
   setup() {
-    return {}
+    return {
+      searchFormConfig,
+      contentTableConfig
+    }
   }
 })
 </script>
