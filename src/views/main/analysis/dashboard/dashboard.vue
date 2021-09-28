1db9d72739ff47d7d8741e4105f78bb3884a80c6
liux <liux1118@qq.com>
2021-09-28 15:59:19 +0800

    feat(dashboard): lineEchart折线图的制作

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -19,7 +19,9 @@
 
     <el-row :gutter="10" class="content-row">
       <el-col :span="12">
-        <lx-card title="分类商品的销量"></lx-card>
+        <lx-card title="分类商品的销量">
+          <line-echart v-bind="categoryGoodsSale" />
+        </lx-card>
       </el-col>
       <el-col :span="12">
         <lx-card title="分类商品的收藏"></lx-card>
@@ -34,20 +36,22 @@ import { useStore } from '@/store'
 
 import LxCard from '@/base-ui/card'
 
-import { PieEchart, RoseEchart } from '@/components/page-echarts'
+import { PieEchart, RoseEchart, LineEchart } from '@/components/page-echarts'
 
 export default defineComponent({
   name: 'dashboard',
   components: {
     LxCard,
     PieEchart,
-    RoseEchart
+    RoseEchart,
+    LineEchart
   },
   setup() {
     const store = useStore()
-    // 发起网络请求
+    // 发起网络请求数据
     store.dispatch('dashboardModule/getDashboardDataAction')
 
+    // 获取数据
     const categoryGoodsCount = computed(() => {
       // map映射
       return store.state.dashboardModule.categoryGoodsCount.map((item: any) => {
@@ -55,8 +59,22 @@ export default defineComponent({
       })
     })
 
+    const categoryGoodsSale = computed(() => {
+      const xLabels: string[] = []
+      const values: any[] = []
+
+      const categoryGoodsSale = store.state.dashboardModule.categoryGoodsSale
+      for (const item of categoryGoodsSale) {
+        xLabels.push(item.name)
+        values.push(item.goodsCount)
+      }
+
+      return { xLabels, values }
+    })
+
     return {
-      categoryGoodsCount
+      categoryGoodsCount,
+      categoryGoodsSale
     }
   }
 })
