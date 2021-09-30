5c3160ccc1db15902993b59d53577e47a5441071
liux <liux1118@qq.com>
2021-09-30 15:43:49 +0800

    feat(service): 更改生产环境API

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -15,7 +15,7 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://lx.org/prod'
+  BASE_URL = '/api'
 } else {
   BASE_URL = 'http://lx.org/test'
 }
