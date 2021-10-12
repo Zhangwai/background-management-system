2571ef8e89b7f797f293143142b9312e0ad1dae9
liux <liux1118@qq.com>
2021-10-12 14:34:23 +0800

    docs(goods): 修改搜索区域

--- src/views/main/product/goods/config/content.config.ts
+++ src/views/main/product/goods/config/content.config.ts
@@ -5,7 +5,7 @@ export const contentTableConfig = {
     { prop: 'name', label: '商品名称', minWidth: '80' },
     { prop: 'oldPrice', label: '原价', minWidth: '60', slotName: 'oldPrice' },
     { prop: 'newPrice', label: '现价', minWidth: '60', slotName: 'newPrice' },
-    { prop: 'desc', label: '描述', minWidth: '60' },
+    // { prop: 'desc', label: '描述', minWidth: '60' },
     { prop: 'status', label: '状态', minWidth: '60', slotName: 'status' },
     { prop: 'imgUrl', label: '商品图片', minWidth: '80', slotName: 'image' },
     {
@@ -32,12 +32,12 @@ export const contentTableConfig = {
       minWidth: '60',
       slotName: 'address'
     },
-    {
-      prop: 'categoryId',
-      label: '分类号',
-      minWidth: '60',
-      slotName: 'categoryId'
-    },
+    // {
+    //   prop: 'categoryId',
+    //   label: '分类号',
+    //   minWidth: '60',
+    //   slotName: 'categoryId'
+    // },
     {
       prop: 'createAt',
       label: '创建时间',
@@ -50,7 +50,7 @@ export const contentTableConfig = {
       minWidth: '150',
       slotName: 'updateAt'
     },
-    { label: '操作', minWidth: '100', slotName: 'handler' }
+    { label: '操作', minWidth: '120', slotName: 'handler' }
   ],
 
   // 是否显示序号
--- src/views/main/product/goods/config/search.config.ts
+++ src/views/main/product/goods/config/search.config.ts
@@ -12,37 +12,19 @@ export const searchFormConfig: IForm = {
   // },
   formItems: [
     {
-      field: 'id',
-      type: 'input',
-      label: 'id',
-      placeholder: '请输入id'
-    },
-    {
       field: 'name',
       type: 'input',
-      label: '用户名',
-      placeholder: '请输入用户名'
-    },
-    {
-      field: 'realname',
-      type: 'input',
-      label: '真实姓名',
-      placeholder: '请输入真实姓名'
-    },
-    {
-      field: 'cellphone',
-      type: 'input',
-      label: '电话号码',
-      placeholder: '请输入电话号码'
+      label: '商品名称',
+      placeholder: '请输入商品名称'
     },
     {
       field: 'enable',
       type: 'select',
-      label: '用户状态',
-      placeholder: '请选择用户状态',
+      label: '商品状态',
+      placeholder: '请选择商品状态',
       options: [
-        { title: '启用', value: 1 },
-        { title: '禁用', value: 0 }
+        { title: '在售', value: 1 },
+        { title: '售空', value: 0 }
       ]
     },
     {
