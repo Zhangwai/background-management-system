a2d8e10db0d70397f27736e6ff590e2019eebfd5
zhangwai <1519214533@qq.com>
2022-01-25 11:50:41 +0800

    fix: fix vue.config,js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,14 @@
 module.exports = {
   // 1.配置方式一：CLI提供的属性
   outputDir: './build',
-  publicPath: '/', //这个必须，引入静态资源需要从根路径引入，否则会找不到静态资源
+  //这个必须，引入静态资源需要从根路径引入，否则会找不到静态资源
+  publicPath: '/',
+  // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: './',
+  // 指定生成的 index.html 的输出路径 (相对于 outputDir)。也可以是一个绝对路径。
+  indexPath: 'index.html',
+  // 默认在生成的静态资源文件名中包含hash以控制缓存
+  filenameHashing: true,
   // 解决跨域问题：在webpack设置devServer => proxy
   devServer: {
     proxy: {
